4949d97cfafe05608c613596086a50524e4dfe8a
AnneGayathri <gayathrianne12@gmail.com>
2025-05-16 15:09:32 +0530

    insert commands

---
+++ EMS/insert_data_ems.sql
@@ -0,0 +1,6 @@
+load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\t_cus_stage_data.csv'
+into table t_cus_stage
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 rows;
